e43fcb18ddec3626343dd67cfac6451624c15a2e
Rene Lochner <school@renelochner.at>
2020-10-11 14:26:37 +0200

    Implement "MahApps.Metro"

--- Vereinsverwaltung/Vereinsverwaltung/MainWindow.xaml.cs
+++ Vereinsverwaltung/Vereinsverwaltung/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MemberManager;
+﻿using MahApps.Metro.Controls;
+using MemberManager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +20,7 @@ namespace Vereinsverwaltung
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         List<Member> members;
         public MainWindow()
